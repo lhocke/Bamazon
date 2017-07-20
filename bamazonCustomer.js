75f892dfdf09a60c9d5b4f4db2f7e5aa145c5860
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-19 17:18:16 -0700

    adding test files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,4 @@
+
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var consoleTable = require('console.table');
@@ -5,8 +6,43 @@ var consoleTable = require('console.table');
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
-	user: "host",
+	user: "root",
 	password: "password",
 	database: "bamazon_db"
 });
 
+var inventory = [];
+var values = []
+
+connection.connect(function(err){
+    if (err) throw err;
+    bamazonStart()
+});
+
+var bamazonStart = function(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        inventory = []
+        for (var i = 0; i < res.length; i++) {
+            // console.log("\nResult" + res[i].id);
+            inventory.push({id: res[i].id, item: res[i].product_name, department: res[i].department_name, price: res[i].price, stock: res[i].stock_quantity});
+        };
+        console.log(inventory)
+        console.table(['id','item','department','price','stock'], inventory);
+        console.log(consoleTable)
+        console.log("running")
+    connection.end()
+    })
+};
+
+// function purchase () {
+//     connection.query("SELECT * FROM products", function(err, res) {
+//         if(err) throw err;
+//         values = [];
+//         for (var i = 0 ; i < res.length ; i++) {
+//             values.push([res[i].id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity])
+//         };
+//         console.log(values)
+//         console.table(['Item ID','Item','Department','Price', 'Stock'], values);
+//     })
+// }
---
+++ customer.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+require('console.table');
+console.table([
+  {
+    name: 'foo',
+    age: 10
+  }, {
+    name: 'bar',
+    age: 20
+  }
+]);
+
+console.table([{"this":"that"}])
