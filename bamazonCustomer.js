46f71c922d2287bf821ab517dbfb0bbf18022e32
lhocke <dylan.ishihara@gmail.com>
2017-07-19 21:11:01 -0700

    adding start of inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,21 +32,38 @@ var bamazonStart = function(){
             // console.log("\nResult" + res[i].id);
             table.push([res[i].id, res[i].product_name, res[i].department_name, '$' + res[i].price, res[i].stock_quantity]);
         };
-        // console.log(inventory)
-    // table.push(inventory)
-    console.log(table.toString());
-    connection.end()
+        console.log(table.toString());
+        letsShop(res)
     })
 };
 
-// function purchase () {
-//     connection.query("SELECT * FROM products", function(err, res) {
-//         if(err) throw err;
-//         values = [];
-//         for (var i = 0 ; i < res.length ; i++) {
-//             values.push([res[i].id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity])
-//         };
-//         console.log(values)
-//         console.table(['Item ID','Item','Department','Price', 'Stock'], values);
-//     })
-// }
+var letsShop = function(items) {
+    inquirer.prompt([
+        {
+            name: "purchase",
+            message: "Which item number would you like to purchase?",
+            type: "input",
+            validate: function(input){
+                if (!input || parseInt(input) > items.length) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        {
+            name: "quantity",
+            message: "How many would you like to purchase?",
+            type: "input",
+            validate: function(input){
+                if (!input) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }
+    ]).then(function)
+}
+    
+    // connection.end()
