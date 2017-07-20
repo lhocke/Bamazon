8c6b86886075e548940534b00b718ff67e09cfd5
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-20 10:03:08 -0700

    store front ant database update now working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,9 +25,9 @@ connection.connect(function(err){
 });
 
 var bamazonStart = function(){
+    // reads data from table products and pushes them to table
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
-        inventory = []
         for (var i = 0; i < res.length; i++) {
             // console.log("\nResult" + res[i].id);
             table.push([res[i].id, res[i].product_name, res[i].department_name, '$' + res[i].price, res[i].stock_quantity]);
@@ -63,7 +63,35 @@ var letsShop = function(items) {
                 }
             }
         }
-    ]).then(function)
-}
+    ]).then(function(response) {
+        var chosenItem = response.purchase -1;
+        var amount = parseInt(response.quantity)
     
-    // connection.end()
+        if (parseInt(amount) > items[chosenItem].stock_quantity) {
+            console.log("Sorry, we don't have that many, please enter a smaller amount");
+            letsShop(items)
+        } else {
+            console.log("Your total is: " + items[chosenItem].price * response.quantity)
+            connection.query("UPDATE products SET stock_quantity = ? WHERE id = ?", [parseInt(items[chosenItem].stock_quantity) - parseInt(response.quantity), items[chosenItem].id], function(err) {
+                if (err) throw err;
+            })
+            inquirer.prompt([
+                {
+                    name: "again",
+                    message: "Would you like to buy something else? (Y/N)",
+                    type: "confirm",
+                    default:false
+                }
+            ]).then(function(response) {
+                switch(response.again){
+                    case true:
+                        letsShop(items)
+                    break;
+                    case false:
+                        console.log("Thanks for shopping! Come again soon!");
+                        connection.end()
+                }
+            })
+        }
+    })
+}
