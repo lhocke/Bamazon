f874bd54a47ece5bf000f6ed924fdd8e2755f5e5
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-20 17:02:53 -0700

    adding update and low inventory functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bamazon
-A CLI store
+
+A CLI store emulator that allows customers to view and purchase items, and for managers to check item listings, see low inventory, add items, and update item details as needed.
+
+#Customer
+
+To run the customer app, open bamazonCustomer.js in node. You will be shown a table of items, asked to make a selection, and asked to specify the quantity of that item that you wish to purchase. If the available stock is lower than your requested quantity you will be shown an error message and asked to choose again, otherwise you will be shown the total for your purchase and asked if you want to purchase another item. If you enter yes, you will be taken back to the table and shown an updated listing from the database. Otherwise you will be shown a thank you message and the program will end.
+
+To run the manager app, open bamazonManager.js in node. You will be asked to choose from a list of four actions; "View Items", "See Low Inventory", "Update Stock", and "Add An Item". The first two will return a table similar to the one the customer receives or one that displays all items with an inventory under 4 based on your choice. Update Stock will allow you to select an item from the list and update the database with the appropriate number of items. Add an item allows you to enter a new item into the database with the name, department, price, and quantity. 
--- bamazonManager.js
+++ bamazonManager.js
@@ -15,8 +15,8 @@ var bamazonStart = function(){
     console.log("\033c")
 
     var table = new Table({
-        head: ['ID', 'Item', 'Department', 'Price', 'In Stock']
-      , colWidths: [4, 20, 20, 20, 20]
+        head: ['ID', 'Item', 'Department', 'Price', 'In Stock'],
+        colWidths: [4, 20, 20, 20, 20]
     });
 
     // reads data from table products and pushes them to table
@@ -44,16 +44,87 @@ var shopManage = function(item) {
                 console.log(table.toString());
             break;
             case "See Low Inventory":
-                lowCheck(item);
+                lowCheck(res);
             break;
             case "Update Stock",
-                update();
+                update(res);
             break;
             case "Add An Item":
-                itemAdd()
+                itemAdd(res)
             break;
         }
     })
 };
 
-var lowCheck = function(item)
+var lowCheck = function(item) {
+    console.log("\033c");
+    var lowTable = new Table({
+        head: ['ID', 'Item', 'Department', 'Price', 'In Stock'],
+        colWidths: [4, 20, 20, 20, 20]
+    });
+    for (var i = 0; i < item.length; i++) {
+        if (item[i].stock_quantity < 4) {
+            lowTable.push(([res[i].id, res[i].product_name, res[i].department_name, '$' + res[i].price, res[i].stock_quantity]))
+        }
+    }
+    console.log(newTable.toString());
+    inquirer.prompt([
+        {
+            name: "action",
+            message: "What would you like to do?",
+            type: "list",
+            choices: ["Update Stock", "Add An Item", "View All Items", "Exit"]
+        }
+    ]).then(function(req){
+        switch(req.action){
+            case "View All Items":
+                console.log("\033c", table.toString());
+                shopManage(res)
+            break;
+            case "Update Stock":
+                update(res);
+            break;
+            case "Add An Item":
+                itemAdd(res)
+            break;
+            case "Exit":
+                process.exit()
+            break;
+        }
+    })
+}
+
+var update = function(a) {
+    console.log("\033c");
+    console.log(table.toString());
+    inquirer.prompt([
+        {
+            name: "product",
+            message: "Which product # would you like to update?",
+            type: "input",
+            validate: function() {
+                product.replace(/\D/g, "")
+                if (!product || parseInt(product) > a.length || product < 1) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        {
+            name: "amount",
+            message: "How many would you like to add?",
+            type: "input"
+            validate: function() {
+                amount.replace(/\D/g, "");
+                if (!amount) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }
+    ]).then(function(action) {
+
+    })
+}
